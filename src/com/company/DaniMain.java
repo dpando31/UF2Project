73f6b84d4cd137b4d7efac1b2f956cc41d9ccf2e
Your Name <you@example.com>
2018-03-16 17:42:15 +0100

    dani_main2

--- src/com/company/DaniMain.java
+++ src/com/company/DaniMain.java
@@ -2,6 +2,12 @@ package com.company;
 
 public class DaniMain {
     public static void main(String[] args) {
-
+        Videojuego juego = new Videojuego();
+        juego.Edad=18;
+        juego.Fabricante="Bluehole";
+        juego.FechaPublicacion=20-12-2017;
+        juego.Nombre= "PlayerUnknowns Battlegrounds";
+        juego.Plataforma="PC";
+        juego.Precio=29;
     }
 }
