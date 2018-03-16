693d09dc5fcb920865f18752db1404e60bc5a9e9
Your Name <you@example.com>
2018-03-16 19:12:00 +0100

    clase developer

---
+++ src/com/company/MainSergi.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import com.company.model.Developer;
+
+public class MainSergi {
+    public static void main(String[] args) {
+        Developer d1 = new Developer();
+        d1.nombreDeveloper = "EA";
+        d1.localizacion = ""
+        d1.mail =
+
+    }
+}
---
+++ src/com/company/model/Developer.java
@@ -0,0 +1,51 @@
+package com.company.model;
+
+public class Developer {
+        //CAMPOS
+        public String nombreDeveloper;
+        public String localizacion;
+        public String mail;
+        public String sitiosWeb;
+        public String fundadores;
+        public String productos;
+        public int numeroEmpleados;
+        public int fechaFundacion;
+
+        //METODOS
+        public void setNombreDeveloper(String nombreDeveloper) {
+                this.nombreDeveloper = nombreDeveloper;
+        }
+
+        public void setLocalizacion(String localizacion){
+                this.localizacion = localizacion;
+        }
+
+        public void setMail(String mail){
+                this.mail = mail;
+        }
+
+        public void setSitiosWeb (String sitiosWeb) {
+                this.sitiosWeb = sitiosWeb;
+        }
+
+        public void setFundadores (String fundadores){
+                this.fundadores = fundadores;
+        }
+
+        public void setProductos (String productos){
+                this.productos = productos;
+        }
+
+        public void setNumeroEmpleados(int numeroEmpleados){
+                this.numeroEmpleados = numeroEmpleados;
+        }
+
+        public void setFechaFundacion(int fechaFundacion) {
+                this.fechaFundacion = fechaFundacion;
+        }
+}
+
+
+
+
+
