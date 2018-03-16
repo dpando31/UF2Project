6b1c9fa5d57b7072f4be43ff9bd8b583fc9fad9e
Your Name <you@example.com>
2018-03-16 18:29:01 +0100

    dani_main2

--- src/com/company/DaniMain.java
+++ src/com/company/DaniMain.java
@@ -1,13 +1,16 @@
 package com.company;
 
+import com.company.model.Videojuego;
+
 public class DaniMain {
     public static void main(String[] args) {
         Videojuego juego = new Videojuego();
-        juego.Edad=18;
-        juego.Fabricante="Bluehole";
-        juego.FechaPublicacion=20-12-2017;
-        juego.Nombre= "PlayerUnknowns Battlegrounds";
-        juego.Plataforma="PC";
-        juego.Precio=29;
+
+        juego.edad=18;
+        juego.fabricante="Bluehole";
+        juego.fechaPublicacion=20-12-2017;
+        juego.nombre= "PlayerUnknowns Battlegrounds";
+        juego.plataforma="PC";
+        juego.precio=29;
     }
 }
