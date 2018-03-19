4d5e3a59984805451a502f88580b4256a94c8f62
Your Name <you@example.com>
2018-03-19 19:29:40 +0100

    dani_main2

--- src/com/company/DaniMain.java
+++ src/com/company/DaniMain.java
@@ -4,23 +4,26 @@ import com.company.model.Videojuego;
 
 public class DaniMain {
     public static void main(String[] args) {
-        Videojuego juego = new Videojuego();
 
-        juego.edad=18;
-        juego.fabricante="Bluehole";
-        juego.fechaPublicacion=20-12-2017;
-        juego.nombre= "PlayerUnknowns Battlegrounds";
-        juego.plataforma="PC";
-        juego.precio=29;
-
-        Videojuego juego2 = new Videojuego();
-
-
-        juego2.setPrecio(29);
-        juego2.setEdad(18);
-        juego2.setFabricante("EA Sports");
-        juego2.setNombre("FIFA18");
-        juego2.setPlataforma("PC");
-        juego2.setFechaPublicacion(1-1-2018);
     }
 }
+
+//
+//    Videojuego juego = new Videojuego();
+//
+//        juego.edad=18;
+//                juego.fabricante="Bluehole";
+//                juego.fechaPublicacion=20-12-2017;
+//                juego.nombre= "PlayerUnknowns Battlegrounds";
+//                juego.plataforma="PC";
+//                juego.precio=29;
+//
+//                Videojuego juego2 = new Videojuego();
+//
+//
+//                juego2.setPrecio(29);
+//                juego2.setEdad(18);
+//                juego2.setFabricante("EA Sports");
+//                juego2.setNombre("FIFA18");
+//                juego2.setPlataforma("PC");
+//                juego2.setFechaPublicacion(1-1-2018);
