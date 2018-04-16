968b63affd73b96c963c141acc48c652baf20615
marc <marc@elpuig>
2018-04-16 20:35:00 +0200

    pantalla crearjuego

--- src/com/company/View/PantallaCrearJuego.java
+++ src/com/company/View/PantallaCrearJuego.java
@@ -11,6 +11,13 @@ public class PantallaCrearJuego {
         System.out.println("Pon el nombre del juego que quieras añadir");
         String nombre = scanner.nextLine();
 
+        System.out.println("Genero del juego");
+        String genero = scanner.nextLine();
+
+        System.out.println("Pon la plataforma del juego");
+        String plataforma = scanner.nextLine();
+
+
 
 
         managerJuego.Crearjuego(nombre);
