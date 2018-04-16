9867efb1df895383cc7780b97cd7e44340b1b694
Your Name <you@example.com>
2018-04-16 20:16:18 +0200

    Prova si le va el Sokar

--- src/com/company/View/PantallaUltimos.java
+++ src/com/company/View/PantallaUltimos.java
@@ -7,9 +7,9 @@ import java.util.Scanner;
 public class PantallaUltimos {
     public void mostrar(ManagerJuego managerJuego) {
         System.out.println("Eston son los ultimos juegos");
-        System.out.println("a)");
-        System.out.println("b)");
-        System.out.println("c)");
-        System.out.println("d)");
+        System.out.println("1)");
+        System.out.println("2)");
+        System.out.println("3)");
+        System.out.println("4)");
     }
 }
